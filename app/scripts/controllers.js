337743b4a2bd1df138da7f597b492ec4a874318c
Thobias Bergström <thobias@rabble.se>
2015-02-18 14:10:49 +0100

    Created basic experiment view and controller, styling

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,5 +1,63 @@
 var mismatchControllers = angular.module('mismatchControllers', []);
 
 mismatchControllers.controller('StartCtrl', ['$scope', function ($scope) {
-    $scope.hej = "Hello!";
+
+  var mousePosition = {'x': 0, 'y': 0};
+
+  // In ms
+  var updatingFrequency = 17;
+
+  $scope.calibration = function() {
+    var delta = 0,
+        deltaSum = 0,
+        deltaSamples = 0;
+
+    document.onmousemove = function(event) {
+      delta = window.performance.now() - delta;
+
+      if(delta < 1000) {
+        deltaSum += delta;
+
+        mousePosition.x = event.clientX;
+        mousePosition.y = event.clientY;
+
+        delta = window.performance.now();
+        deltaSamples++;
+      }
+
+      if(deltaSamples >= 50) {
+        document.onmousemove = null;
+
+        updatingFrequency = deltaSum/deltaSamples;
+
+        console.log('Updating frequency set to ' + updatingFrequency + ' ms (' + 1000/updatingFrequency + ' hz)');
+        alert(1000/updatingFrequency + ' hz');
+      }
+
+    };
+
+  };
+
+  $scope.startTracking = function() {
+    var tracking,
+        trackingData = '';
+
+    document.onmousemove = function(event) {
+      mousePosition.x = event.clientX;
+      mousePosition.y = event.clientY;
+    };
+
+    tracking = setInterval(function () {
+     trackingData += mousePosition.x + ', ' + mousePosition.y + ', ' + window.performance.now() + '\n';
+    }, updatingFrequency);
+
+    $scope.stopTracking = function() {
+      console.log(trackingData);
+      clearInterval(tracking)
+      document.onmousemove = null;
+    };
+
+  };
+
+
 }]);
