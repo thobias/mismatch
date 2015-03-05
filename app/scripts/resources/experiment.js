e05e11c6fae697b0e69d5031155bc74fdf6bb6dd
Thobias Bergström <thobias@rabble.se>
2015-03-05 13:22:29 +0100

    Updated links to work online

--- app/scripts/resources/experiment.js
+++ app/scripts/resources/experiment.js
@@ -12,78 +12,78 @@ angular.module('mismatchResources').factory('experiment', [function() {
     'preTrials': [
       {
         'id': 1,
-        'image1': '/images/faces/012AA.jpg',
-        'image2': '/images/faces/012BB.jpg',
+        'image1': 'images/faces/012AA.jpg',
+        'image2': 'images/faces/012BB.jpg',
         'manipulated': false
       },
       {
         'id': 2,
-        'image1': '/images/faces/013AA.jpg',
-        'image2': '/images/faces/013BB.jpg',
+        'image1': 'images/faces/013AA.jpg',
+        'image2': 'images/faces/013BB.jpg',
         'manipulated': false
       },
       {
         'id': 3,
-        'image1': '/images/faces/015AA.jpg',
-        'image2': '/images/faces/015BB.jpg',
+        'image1': 'images/faces/015AA.jpg',
+        'image2': 'images/faces/015BB.jpg',
         'manipulated': false
       },
     ],
     'trials': [
       {
         'id': 1,
-        'image1': '/images/faces/001AA.jpg',
-        'image2': '/images/faces/001BB.jpg',
+        'image1': 'images/faces/001AA.jpg',
+        'image2': 'images/faces/001BB.jpg',
         'manipulated': false
       },
       {
         'id': 2,
-        'image1': '/images/faces/002AA.jpg',
-        'image2': '/images/faces/002BB.jpg',
+        'image1': 'images/faces/002AA.jpg',
+        'image2': 'images/faces/002BB.jpg',
         'manipulated': false
       },
       {
         'id': 3,
-        'image1': '/images/faces/003AA.jpg',
-        'image2': '/images/faces/003BB.jpg',
+        'image1': 'images/faces/003AA.jpg',
+        'image2': 'images/faces/003BB.jpg',
         'manipulated': true
       },
       {
         'id': 4,
-        'image1': '/images/faces/004AA.jpg',
-        'image2': '/images/faces/004BB.jpg',
+        'image1': 'images/faces/004AA.jpg',
+        'image2': 'images/faces/004BB.jpg',
         'manipulated': false
       },
       {
         'id': 5,
-        'image1': '/images/faces/005AA.jpg',
-        'image2': '/images/faces/005BB.jpg',
+        'image1': 'images/faces/005AA.jpg',
+        'image2': 'images/faces/005BB.jpg',
         'manipulated': true
       },
       {
         'id': 6,
-        'image1': '/images/faces/006AA.jpg',
-        'image2': '/images/faces/006BB.jpg',
+        'image1': 'images/faces/006AA.jpg',
+        'image2': 'images/faces/006BB.jpg',
         'manipulated': false
       }
     ],
     'postTrials': [
       {
         'id': 1,
-        'image1': '/images/faces/001AA.jpg',
-        'image2': '/images/faces/001BB.jpg',
+        'image1': 'images/faces/001AA.jpg',
+        'image2': 'images/faces/001BB.jpg',
         'manipulated': false
       },
       {
         'id': 2,
-        'image1': '/images/faces/005AA.jpg',
-        'image2': '/images/faces/005BB.jpg',
+        'image1': 'images/faces/005AA.jpg',
+        'image2': 'images/faces/005BB.jpg',
         'manipulated': false
       },
       {
         'id': 3,
-        'image1': '/images/faces/004AA.jpg',
-        'image2': '/images/faces/004BB.jpg',
+        'image1': 'images/faces/004AA.jpg',
+        'image2': 'images/faces/004BB.jpg',
         'manipulated': false
       }
     ]
