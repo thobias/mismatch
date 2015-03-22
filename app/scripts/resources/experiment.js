98f5f7894678ac1ca6eaff5675df88579c8bccfd
Thobias Bergström <thobias@rabble.se>
2015-03-22 22:28:04 +0100

    Now generating experiment at runtime using Marsenne Twister randomization of trials

--- app/scripts/resources/experiment.js
+++ app/scripts/resources/experiment.js
@@ -1,16 +1,53 @@
 angular.module('mismatchResources').factory('experiment', [function() {
-  // Generate on runtime
+
+  // Generate a trial
+  var trial = function(id, manipulated) {
+    return {
+      'id': id,
+      'image1': 'images/faces/'+id+'AA.jpg',
+      'image2': 'images/faces/'+id+'BB.jpg',
+      'manipulated': manipulated || false
+    };
+  };
+
+  // Random number generator using Mersenne Twister algo
   var engine = Random.engines.mt19937().autoSeed();
-  // 8 potentially manipulated
-  // Pick 4
-  // Randomize order
-  // Add to array of trials
-  // Add to postTrials
-  return {
+  // 8 potentially manipulated trials
+  var available = [trial('006', true), trial('007', true), trial('008', true), trial('009', true), trial('010', true), trial('011', true), trial('012', true), trial('013', true)];
+  // Pick 4 of them and shuffle the order
+  var chosen = Random.shuffle( engine, Random.sample(engine, available, 4) );
+  // 16 ordinary trials (non-manipulated)
+  var ordinaryTrials = [trial('014'), trial('015'), trial('016'), trial('017'), trial('018'), trial('019'), trial('020'), trial('021'), trial('022'), trial('023'), trial('024'), trial('025'), trial('026'), trial('027'), trial('028'), trial('029')];
+  // Shuffle the trials
+  var trials = Random.shuffle(engine, angular.copy(ordinaryTrials));
+  // Splice in our manipulated trials at random places but avoiding back-to-back manipulations
+  var validIndexes = [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+  $.each(chosen, function() {
+    var index = Random.pick( engine, validIndexes );
+    trials.splice(index, 0, this);
+    // Remove chosen index and the ones surrounding it from validIndexes
+    // (ie. if the value 12 is chosen, remove 11, 12 and 13)
+    validIndexes.splice( validIndexes.indexOf(index) - 1, 3);
+  });
+
+  // Choose post-trials
+  var postTrials = Random.sample( engine, ordinaryTrials, 4);
+  // Add chosen manipulated trials
+  postTrials = postTrials.concat(chosen);
+  // Shuffle
+  postTrials = Random.shuffle( engine, postTrials );
+
+  // Choose pre-trials
+  var preTrials = [trial('001'), trial('002'), trial('003'), trial('004'), trial('005')];
+  // Shuffle
+  preTrials = Random.shuffle( engine, preTrials );
+
+  // Complete experiment object
+  var experiment = {
     'userId': Date.now() + Random.uuid4(engine),
     'experimentId': 1,
     'user': {
-      'sex': 'm',
+      'sex': 'male',
       'age': 25,
       'device': 'mouse',
       'devicePosition': 'right',
@@ -20,84 +57,10 @@ angular.module('mismatchResources').factory('experiment', [function() {
         'height': screen.height
       }
     },
-    'preTrials': [
-      {
-        'id': 1,
-        'image1': 'images/faces/012AA.jpg',
-        'image2': 'images/faces/012BB.jpg',
-        'manipulated': false
-      },
-      {
-        'id': 2,
-        'image1': 'images/faces/013AA.jpg',
-        'image2': 'images/faces/013BB.jpg',
-        'manipulated': false
-      },
-      {
-        'id': 3,
-        'image1': 'images/faces/015AA.jpg',
-        'image2': 'images/faces/015BB.jpg',
-        'manipulated': false
-      },
-    ],
-    'trials': [
-      {
-        'id': 1,
-        'image1': 'images/faces/001AA.jpg',
-        'image2': 'images/faces/001BB.jpg',
-        'manipulated': false
-      },
-      {
-        'id': 2,
-        'image1': 'images/faces/002AA.jpg',
-        'image2': 'images/faces/002BB.jpg',
-        'manipulated': false
-      },
-      {
-        'id': 3,
-        'image1': 'images/faces/003AA.jpg',
-        'image2': 'images/faces/003BB.jpg',
-        'manipulated': true
-      },
-      {
-        'id': 4,
-        'image1': 'images/faces/004AA.jpg',
-        'image2': 'images/faces/004BB.jpg',
-        'manipulated': false
-      },
-      {
-        'id': 5,
-        'image1': 'images/faces/005AA.jpg',
-        'image2': 'images/faces/005BB.jpg',
-        'manipulated': true
-      },
-      {
-        'id': 6,
-        'image1': 'images/faces/006AA.jpg',
-        'image2': 'images/faces/006BB.jpg',
-        'manipulated': false
-      }
-    ],
-    'postTrials': [
-      {
-        'id': 1,
-        'image1': 'images/faces/001AA.jpg',
-        'image2': 'images/faces/001BB.jpg',
-        'manipulated': false
-      },
-      {
-        'id': 2,
-        'image1': 'images/faces/005AA.jpg',
-        'image2': 'images/faces/005BB.jpg',
-        'manipulated': true
-      },
-      {
-        'id': 3,
-        'image1': 'images/faces/004AA.jpg',
-        'image2': 'images/faces/004BB.jpg',
-        'manipulated': false
-      }
-    ]
+    'preTrials': preTrials,
+    'trials': trials,
+    'postTrials': postTrials
   };
-
+  console.log(experiment);
+  return experiment;
 }]);
